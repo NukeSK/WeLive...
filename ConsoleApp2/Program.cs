394eedaddc4c4b06c6dec86bbc26cb9d98bc7e25
Usuario <Usuario@DESKTOP-SIABH8K>
2023-09-08 17:49:44 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We live...");
+            Console.ReadKey();
+            Console.WriteLine("We love...");
+            Console.ReadKey();
+            Console.WriteLine("We lie...");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine(".............   ...............................         .........,cdO0KKKOdlc,.............");
+            Console.WriteLine("...'''',,,'...    ..............................      ........,ok0NMMMMMMWWWWNOo,................");
+            Console.WriteLine("........''''...      .................   .      .   ........':OWMMWWMMMMMWMMWWWWKl'................");
+            Console.WriteLine("    ...........      .................  .            ......';OWWMMMWMMMMWWNNWWWWWNk;......'''''....");
+            Console.WriteLine("     .........        .........                     .....'xWMMMMMMMMWWWNNXNNNWWWWKc.......''.....");
+            Console.WriteLine("..............         .........                     .....'dNWMMMMMMMWWWNNNNXXKKXXXNNWNk,.'';::,'....");
+            Console.WriteLine("...... ........         .....              ...... .......'xNMMMMMMMMMWWWWWNXXXKKKXNNNWWNk;.,;::;,....");
+            Console.WriteLine("        ........        ....              ..............,xNMMMMMMMMMWWWWNNXXXXXXXXXNNNWWWKo;',,,'....");
+            Console.WriteLine("          ......          .               ............'l0WMMMMMMMMMMMMWWNNNNNXXNNNNWWWWWWWWWKd;........");
+            Console.WriteLine("    .                                     .....''',:kNMMMMMMMMMMMMMMMMMMMMMMWWWWMMMMMMMMMMMWWMXd,.");
+            Console.WriteLine("                                 ..............',cxXWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWMN0c.");
+            Console.WriteLine("              .                .............'',ckNMMMMMMMMMMMMWWMMMMMMMMMMMMMMWMMMMMMMMMMMMMMMMMXkc,.");
+            Console.WriteLine("           ......              .......''',,,;ckNWMMMMMMMMMWMMWWMMMWWWWWNNNNNWWWWWWWNWWWWWWMMMMMMWWWNOc.");
+            Console.WriteLine("          ......               ........';;:lONWMMMWWWWNXXKK0OkkkkkkkkxxdddxxxkxxkkOO00KKKXNNNWWWWWWWWW0c.");
+            Console.WriteLine("           .                     .....',,cOXNXK0OOOkdolcc:::;;;,,,,,,,''',''..'..'';:::cclodxkO0KXXNNWWNx'.");
+            Console.WriteLine("                                  .....'l0KOdoc:;;,'''.............. .....          .........'',:ldk0KNWNk;.");
+            Console.WriteLine("                  ......            ...:xxl:;;,'......        .....                               ..':ok0KN0;..");
+            Console.WriteLine("                 ........            ..cdl:;,.......                                                ..,clkKl...");
+            Console.WriteLine("                  ........           ..,llc,......                                                   ...'co,...");
+            Console.WriteLine("                      .....     ...    ..:c:,......                                                   ..;c:.... . ..");
+            Console.WriteLine("                .        ..     ...    ...';;;,;::,.   ........                                    ....,:;....... ..    ........");
+            Console.WriteLine("                    .......             .......',,'.   ..';;;,...               ..  ..           .....','.......    ............");
+            Console.WriteLine("                    ........             ......'''......,d00kl,..........    ............... ...',;;,,,'........................");
+            Console.WriteLine("                        ....               ...'',,'.';:dOXWNOl,''';::;,,,'....................,:cclc,''''...''...............'''");
+            Console.WriteLine("                         ........   ..      ...''....c0XXK0kdc:;,:x0Odc;,''...........''''.',:ldl;,,,',:ldl:;.............''',;;");
+            Console.WriteLine("                        .................    .......'xKd:,,'..':cokkxdoc;,''..'''',,,;::;'',,,;;,''',,;;;lxxc,;;,,,''''''''',;::");
+            Console.WriteLine("                    ..................''............'lxl:,....,lool::::;;,,,,,,,,;:::c:;,,,'''''.''',,,'':xxl:::::;;;;;,''',;:::");
+            Console.WriteLine("                 ...........''''.....',;;;,'........':lol:,'',;;,,;;;;;,,;;::;;;:clc:;,,;;;;;;;;;,;;;;;''cxklccc::::cc:;,'',;;;:");
+            Console.WriteLine(".....   .................',,,,''......',;::,'......',,,:c;;;',;;,,,,;;;;:::c::;;;:cc:,'',,;;;;;::cc::;;;;'':dOdccc::::cc:;;;,;;;;;");
+            Console.WriteLine(".......................''',,,,,'.....'''',,,'......''.......'::cccccc:ccc;,'.',;:c;,,;,''''',;:::;,,;:,.':x0kolc:;;;::;;;;;;;;;;");
+            Console.WriteLine("............ .........'''',,,,,'...',,,,,,,,,'................';:::;;,,cl:'...';:::;,',,,,,,;:;,,,',;;,..:dxkOOxc;;,,,,,,;,,,,,''");
+            Console.WriteLine("............  .........''''''''...',;;;;;:::;,....................',;coxo;,..',;;'.,cl:;,,,,;,'.''..','.,:;cdk0KOc,''',,,,'''...");
+            Console.WriteLine("...................................',,,;;:cc;,'....................;kKKOo:;:;,,,'',;cloolc'....',.....,,,'',;coxOd;','''........");
+            Console.WriteLine("......................................'',;::;;,'..................,xXXXKkddoc:;;;:::::coddc'.........,,'.....',:odN'''..........");
+            Console.WriteLine("  ...   ........    .....................',;;;;;,'..............,lOXNNNX0kdooool::c:;,,;:loo:..... .,'........';cl;''...........");
+            Console.WriteLine("        .......        ...................',,,,;;,'...........;dkkOXNX0kxdxxdolc:;,'....';:ldoc;,. ..   ......';c;...............");
+            Console.WriteLine("       .......         .....................'',,,;,''.....;cldxdooOXKkooxxdolc:;,..    ....,;:llol'.... .....';c;...............");
+            Console.WriteLine("      .........           ..................'',,;:::;,'.;dOOxolc::d0kdxxxolc:;,'...   .'......',cxd,.'''',,,;c;'.........''''...");
+            Console.WriteLine("      ..........          ............'''''',,,;:cc:;;:oOOxoll;''cOkxO00kdolc;,'.......;'.    .',:oo;''',,,,''..'''''''',,,,,,''");
+            Console.WriteLine("     ..........            .............'''',,;;:::ldOKX0dcc:;,;:dxdk00Oxdlc::;,''..''',:'.     .':odl;'.......'''''''''',,,,',,");
+            Console.WriteLine("   ............              .............''',,;cdOKNNKKKOo,'',:lodkkxxxdolc:;,..''''''':dx:     ..,:lolc:;,..''...............'");
+            Console.WriteLine("      ......     .            ...     ......'',dKNKOOkxxkkd:'',:coxkxxxdolcc::;;;,,,,,;cokOc.     ...';coolll:;'................");
+            Console.WriteLine("        ..............                     ...cOKOo;;;;,:do;'';:cxOkxxxxdolcc:::::llodxkkO0x;...  .....,:c:;cldl..........     .");
+            Console.WriteLine("        ......'''''....                     .'oOOo,,,,,;cl:,';:lok0kkkdddollllodkOOO0OOO0KNW0c:,......''.',;:::;'....';;,''.. ..");
+            Console.WriteLine("          .............                    . 'clc,',;;;;;;:;;cldx0XK00kdxxxkkkOKXKXKKNNNWWWWNo'...   ..'..',:clc,',;:::;;,''....");
+            Console.WriteLine("             ..........                      ..c:'',;:cccclclodxk0NNXKKK0KXKKX00KNNNWWWWWWNNKc.....    .,;;::cc:;;:;;,..'..',,''");
+            Console.WriteLine("              ...',,,'..                      .';;;;:ccloooloodxxxk0KXNXOO0OO0KNWMWNNNNNNXKKx,.,,......';:cc:clc;,,,;'. .',;;;;;");
+            Console.WriteLine("           ...',;;;;;,'..                    ....'',:ccllllcccllollccxKK0OOO0NWWWNXXKK0KK00KOkO0KOo;:l:;;:ccc:;;;,..''.';::cllll");
+            Console.WriteLine("         ....'',,,,''''...                .......',::ccccc::::::::;,,,lO000XNWNNXKKK0OOOkO000KK0KXX0dc;;cll:;,,;c:,,;:cllllllolc");
+            Console.WriteLine("      ........''''........             ...',;;:;;;:cclc::::::::;;;,'...;x0XWNNNX00KK0kkkkkkxxxddxkO0klclooc::::;::::cclllllloolc");
+            Console.WriteLine("        .......'''......         ......',;::clllllllllc:::::::;;,,,...  .lXWWNXK000OOOkxkxxkkdodxdxOOOxxdollllclolllc:ccllllodol");
+            Console.WriteLine("           ..'',,,,'.....         .....,;:clodddoollcc:::ccccc:;;,...    :KNXNNNXKK0Okdlc:ck0kxxxxkOOkOkxkxxxdoolllllllcccccllc:");
+            Console.WriteLine("        .....';:;;;,'......      .......,;codxxxdolc::::::clll:;,'..     'kKKXNWWN0dl:;;cd0NX0kkkkkOOkK0xddolcccccc::;;;,,,,,,''");
+            Console.WriteLine("    ...........;c:;,'........  .........';codxxxxdlc::;;;::cllc:;;'..    .kXKXNNNNd..:x0KXWNX0OOOOxoxkOOoclc:;;;,,'.......'.....");
+            Console.WriteLine("     ..........',:;,...... ......',;;;;;;:codxxxdolc::;;;;:oxOOOOOkdl:,,:xXNXXXXXKxok0KXNWNKOkkkkkxollldl;,'....................");
+            Console.WriteLine("     .......'';:cclllolcccc:cc:;;:;;;,,'......',:c:;,,,;;::clddoodxxkOOO0KK0OOkxdlc;,'...              .     ............ ....");
+            Console.WriteLine("     .....,,:cccccclllllccllc:::cllc:;;,''........'',:oxkkkxxxkkOOO00KKKKX0XK0KXK00Od:...                  ..     ............");
+            Console.ReadKey();
+        }
+    }
+}
